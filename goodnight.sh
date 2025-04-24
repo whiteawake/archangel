c1489f954e8873c6334dee29871e08b0b4af8eca
Asha <156074389+whiteawake@users.noreply.github.com>
2025-04-24 20:54:53 +0000

    Update goodnight.sh

--- goodnight.sh
+++ goodnight.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 veracrypt -d
 if [[ $? -eq 0 ]]; then
-  #paplay ~/Music/file.wav &
+  #pw-play ~/Music/file.wav &
   sudo pacman -Syuv --noconfirm && \
   sudo pacman -Scv --noconfirm && \
   yay -Yc --noconfirm && \
