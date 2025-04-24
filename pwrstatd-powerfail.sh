e9ae17904ff92528af59c5ab3722f8c3e1ac3b5d
Asha <156074389+whiteawake@users.noreply.github.com>
2025-04-24 19:36:23 +0000

    Update pwrstatd-powerfail.sh

--- pwrstatd-powerfail.sh
+++ pwrstatd-powerfail.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root."
+    exit 1
+fi
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+logdir="/var/log/UPS"
+if [ ! -d "$logdir" ]; then
+    mkdir -p "$logdir"
+fi
 logfile="/var/log/UPS/ups_powerfail-${timestamp}.log"
 logstamp() {
 	echo "[$(date +"%Y-%m-%d %H:%M:%S")] $1" >> "$logfile"
 }
 logstamp "Mains power supply disconnected. Initiating power failure log and alert..."
-logstamp "Notifying user..." && systemd-run --machine=asha@.host --user notify-send --expire-time=0 --urgency=critical "WARNING: Mains power supply has disconnected." --icon=battery-caution --category=system.power &
-logstamp "Playing alarm..." && machinectl shell --uid=asha .host /usr/bin/pw-play /home/asha/Music/Samples/a300_fire_alarm.wav &
+logstamp "Notifying user..." 
+systemd-run --machine=asha@.host --user notify-send --expire-time=0 --urgency=critical "WARNING: Mains power supply has disconnected." --icon=battery-caution --category=system.power &
+if [ $? -eq 0 ]; then
+	warnpid=$!
+	logstamp "User notified (PID: $warnpid)."
+else
+	logstamp "Unable to visually notify user."
+fi
+logstamp "Playing alarm..."
+machinectl shell --uid=asha .host /usr/bin/pw-play /home/asha/Music/Samples/a300_fire_alarm.wav &
+if [ $? -eq 0 ]; then
+	alarmpid=$!
+	logstamp "Alarm ringing (PID: $alarmpid)."
+else
+	logstamp "Unable to sound alarm."
+fi
 logstamp "Preemptive filesystem sync..."
 if sync; then
-	logstamp "Preemtive sync complete."
+	logstamp "Preemptive sync complete."
 else
-	exit_code=$?
-	logstamp "WARNING: Sync command failed with exit code $exit_code. Data not secured."
+	exitcode=$?
+	logstamp "WARNING: Sync command failed with exit code $exitcode. Data not secured."
 fi
-logstamp "Power failure log sequence completed. Protective shutdown sequence will initiate at <35% UPS battery power."
+logstamp "Power failure log sequence completed. Protective shutdown sequence will initiate at <35% UPS battery power or 540 seconds remaining runtime capacity."
 exit 0
