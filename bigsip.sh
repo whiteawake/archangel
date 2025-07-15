18ebf8d73184a6b6217d26eb1bf8988470863306
Asha <asha.amaranti@protonmail.com>
2025-07-15 23:42:12 +0800

    Update bigsip.sh

--- bigsip.sh
+++ bigsip.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo pacman -Syu --noconfirm
-yay -Sc --noconfirm
-yay -Yc --noconfirm
-yay --aur --cleanafter --noconfirm
-paccache -rk1 -ruk0
-paccache -rk0
+sudo pacman -Syuv --noconfirm
+yay -Yc --noconfirm && yay -Sua --noconfirm
+paccache -rk1 && paccache -ruk0
